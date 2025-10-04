0e9b6ddb125291fb99e5c6f2b86b3996380e9c13
MaksymKhatskevych <KhatskevychMaksym@gmail.com>
2025-10-05 00:39:19 +0300

    for include front

--- backend/main_page/admin.py
+++ backend/main_page/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django import forms
 from mptt.admin import MPTTModelAdmin
 from .models import Header, BackgroundVideo, AboutMe, ServiceCategory, ServiceFeature
 from ckeditor_uploader.widgets import CKEditorUploadingWidget
